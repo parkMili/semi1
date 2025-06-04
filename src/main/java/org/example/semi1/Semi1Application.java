8662ef229be926acd13b1b23e1b19a9b5bf58cc6
parkmili <mimi.151226@gmail.com>
2025-06-04 10:17:03 +0900

    Initial commit

---
+++ src/main/java/org/example/semi1/Semi1Application.java
@@ -0,0 +1,13 @@
+package org.example.semi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/semi1/Semi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.semi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
